01a6162fcf77f401e275ef5cf5b5bc264508ecfd
tttduong <tttduong23@gmail.com>
2024-04-23 06:35:32 +0700

    add button khum đượt

--- src/Viewer/GameFrame.java
+++ src/Viewer/GameFrame.java
@@ -6,9 +6,12 @@ import javax.swing.*;
 
 public class GameFrame extends JFrame {
     LoadData loadData = new LoadData();
-
+GamePanel gamePanel ;
     public GameFrame (){
         loadData = new LoadData();
+
+        add(gamePanel= new GamePanel(9,9,10));
+
         setIconImage(loadData.getListImage().get("title"));
         pack();
         setLocationRelativeTo(null);
--- src/Viewer/GamePanel.java
+++ src/Viewer/GamePanel.java
@@ -1,5 +1,34 @@
 package Viewer;
 
-public class GamePanel {
-    
+import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import static java.awt.AWTEventMulticaster.add;
+
+public class GamePanel extends JPanel{
+    private JPanel p1,p2;
+    private JButton[][] arrayButton;
+    public GamePanel (int w, int h, int boom){
+        arrayButton = new JButton[w][h];
+//        p1 = new JPanel();
+//        p2 = new JPanel();
+//
+//        p1.setLayout(new BorderLayout());
+//        p2.setLayout(new GridLayout());
+
+        add(p1 = new JPanel());
+        add(p2 = new JPanel(new GridLayout(w,h)));
+        for (int i =0 ; i < arrayButton.length; i++){
+            for (int j =0; j< arrayButton.length; j++){
+                p2.add(arrayButton[i][j] = new JButton());
+            }
+        }
+
+        this.setLayout(new BorderLayout());
+
+
+    }
+
+
 }
