b86204fa75d0fd857692f9867e666ef8f82d9f16
tttduong <tttduong23@gmail.com>
2024-04-23 07:14:20 +0700

    add button successfully

--- src/Viewer/GamePanel.java
+++ src/Viewer/GamePanel.java
@@ -11,21 +11,16 @@ public class GamePanel extends JPanel{
     private JButton[][] arrayButton;
     public GamePanel (int w, int h, int boom){
         arrayButton = new JButton[w][h];
-//        p1 = new JPanel();
-//        p2 = new JPanel();
-//
-//        p1.setLayout(new BorderLayout());
-//        p2.setLayout(new GridLayout());
 
-        add(p1 = new JPanel());
+        add(p1 = new JPanel(new BorderLayout()));
         add(p2 = new JPanel(new GridLayout(w,h)));
+
         for (int i =0 ; i < arrayButton.length; i++){
             for (int j =0; j< arrayButton.length; j++){
                 p2.add(arrayButton[i][j] = new JButton());
             }
         }
 
-        this.setLayout(new BorderLayout());
 
 
     }
